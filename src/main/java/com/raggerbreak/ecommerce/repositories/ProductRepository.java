060f6cc669b1d3dc4ec7d320793875db09c7d255
RaggerLtp <ragger.break@gmail.com>
2021-05-02 13:39:08 +0200

    add cross origin

--- src/main/java/com/raggerbreak/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/raggerbreak/ecommerce/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.raggerbreak.ecommerce.repositories;
 
 import com.raggerbreak.ecommerce.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
