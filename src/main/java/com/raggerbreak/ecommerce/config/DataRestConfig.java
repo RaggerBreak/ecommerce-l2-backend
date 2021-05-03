92ce6df4bae2b9112f5c2d3f35c444169741560d
RaggerLtp <ragger.break@gmail.com>
2021-05-03 22:04:04 +0200

    expose entity ids for entity/domain types

--- src/main/java/com/raggerbreak/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/raggerbreak/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,25 @@ package com.raggerbreak.ecommerce.config;
 
 import com.raggerbreak.ecommerce.domain.Product;
 import com.raggerbreak.ecommerce.domain.ProductCategory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+@RequiredArgsConstructor
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,5 +37,27 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        //get entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // expose entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+
     }
 }
